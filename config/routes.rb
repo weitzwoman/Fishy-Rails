534d39ea03ad27ca7b78427ed1ff762f52ce6690
Tracie Weitzman <tlweitzman@gmail.com>
2017-02-17 10:15:06 -0800

    add app models and spec models for products and comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   root to: "home#index"
   devise_for :users
 
+  resources :users do
+    resources :products
+  end
+
+  resources :users do
+    resources :tags
+  end
+
+  # resources :products do
+  #
+  # end
 end
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+describe Comment do
+  it { should belong_to :user }
+  it { should belong_to :product }
+end
