f05d6866f0d821ed96425df832447abc758bee6d
Tracie Weitzman <tlweitzman@gmail.com>
2017-02-17 10:53:27 -0800

    add code to product views pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
+  before_filter :authenticate_user!
   def index
-    if current_user
-      @user = current_user.id
-    end
+    @products = Product.all
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,18 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
 
+  def new
+  end
+
   def create
-    @user = User.create(user_params)
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the site!"
+      redirect_to "/"
+    else
+      flash[:alert] = "Problem creating your account!"
+      redirect_to :back
+    end
   end
 
 private
