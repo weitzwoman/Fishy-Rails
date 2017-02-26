47de167c05b7049ee70f922cb258924884b8b6e2
Tracie Weitzman <tlweitzman@gmail.com>
2017-02-25 22:39:44 -0800

    add passing spec for comments error

--- spec/features/add_comment_pages_spec.rb
+++ spec/features/add_comment_pages_spec.rb
@@ -10,4 +10,13 @@ describe "add comments" do
     click_on "Create Comment"
     expect(page).to have_content 'Something'
   end
+
+  it "will show errors if basic user enter blank field for comments" do
+    user = FactoryGirl.create(:user)
+    product = FactoryGirl.create(:product)
+    login_as(user)
+    visit new_product_comment_path(product)
+    click_on "Create Comment"
+    expect(page).to have_content 'Please try again'
+  end
 end
